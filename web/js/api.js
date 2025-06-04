a40f44cdb5bc1abf8ec275dec2bf23e170e6d4be
daniil <hynslei@ya.ru>
2025-06-04 11:18:32 +0300

    v halate na yachte

--- web/js/api.js
+++ web/js/api.js
@@ -1,5 +1,5 @@
-//const API_BASE_URL = "https://lab.somerka.ru";
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://lab.somerka.ru";
+//const API_BASE_URL = "http://localhost:8080";
 
 class ApiClient {
   constructor() {
